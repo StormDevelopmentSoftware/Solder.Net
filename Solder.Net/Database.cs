b4aa8ad757d561fa1e0c1f43bccd675f4ec37eaa
Animadoria <me@animadoria.cf>
2019-12-30 18:29:46 +0000

    First commit

---
+++ Solder.Net/Database.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace Solder.Net
+{
+    public class Database : IDisposable
+    {
+        public MySqlConnection Connection;
+
+        public Database(string connectionString)
+        {
+            Connection = new MySqlConnection(connectionString);
+            Connection.Open();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
